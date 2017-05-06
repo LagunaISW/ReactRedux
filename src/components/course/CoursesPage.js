82631fb17c5b9be0e7c1a3a56b25ff1dc2c2fc97
Jhonatan Laguna <jhonlpdev@gmail.com>
2017-05-06 11:30:08 -0700

    Async Writes in Redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,25 +1,35 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import { connect } from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
-  constructor(props, context){
+  constructor(props, context) {
     super(props, context);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
-  courseRow(course, index){
+  courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
   }
 
-  render(){
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
+
+  render() {
     const {courses} = this.props;
 
-    return(
+    return (
       <div>
         <h1>Courses</h1>
-        <CourseList courses={courses} />
+        <input type="submit"
+               value="Add Course"
+               className="btn btn-primary"
+               onClick={this.redirectToAddCoursePage}/>
+        <CourseList courses={courses}/>
       </div>
     );
   }
@@ -37,7 +47,7 @@ function mapStateToProps(state, ownProps) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return{
+  return {
     actions: bindActionCreators(courseActions, dispatch)
   };
 }
